95de2895d5d9efc25a719457ea2b5bdb5924881d
Tim Schmelmer <tim.schmelmer@gmail.com>
2017-04-09 17:11:33 -0600

    Updating the example to have more 'flow'

--- example.rb
+++ example.rb
@@ -1,25 +1,34 @@
 require "dotenv"
 Dotenv.load ".env"
 
-schedules = AlertForJira::PagerDuty.get_schedules()
-user_ids = schedules[0].users.map(&:id)
-
-users = user_ids.map { |id| AlertForJira::PagerDuty.get_user(id) }
-pd_emails = users.map(&:email)
-
+### Getting all JIRA users to whom JIRA issues are assigned in a given project
 issues = AlertForJira::Jira.issues_in_project(name: "ACME")
 assignees = issues.map(&:assignee)
 jira_issues_emails = assignees.map(&:emailAddress)
 
+### Getting all JIRA users whom are part of a given project
 project = AlertForJira::Jira.project(name: "ACME")
 jira_project_users = project.users
 jira_user_emails = project.users.map(&:emailAddress)
 
-all_on_call_issues = AlertForJira::Jira.on_call_issues(project_name: "ACME", all: true)
-open_on_call_issues = AlertForJira::Jira.on_call_issues(project_name: "ACME")
-
+# Getting a user by key -- easy with `jira-ruby`
 jira_timbo = AlertForJira::Jira.user(key: "tim_schmelmer")
+# Getting a user by email address -- not part of `jira-ruby`, needs using RestClient
 jira_timbo_rest = AlertForJira::Jira.user_by_email("tim_schmelmer@hotmail.com")
 
-issue = AlertForJira::Jira.assign_issue_to_user(user_email: jira_timbo_rest.emailAddress,
-      issue: all_on_call_issues.first)
+### Switching all open "On-Call" issues to be assigned to the oncall
+schedules = AlertForJira::PagerDuty.get_schedules()
+user_ids = schedules[0].users.map(&:id)
+
+users = user_ids.map { |id| AlertForJira::PagerDuty.get_user(id) }
+on_call_emails = users.map(&:email)
+primary_email = on_call_emails[0]
+
+
+open_on_call_issues = AlertForJira::Jira.on_call_issues(project_name: "ACME")
+#all_on_call_issues = AlertForJira::Jira.on_call_issues(project_name: "ACME", all: true)
+
+open_on_call_issues.map do |issue|
+  AlertForJira::Jira.assign_issue_to_user(user_email: primary_email,
+        issue: issue)
+end
